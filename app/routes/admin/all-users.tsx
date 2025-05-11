73c583adef10a18ad59e7f731a7d4741515ea40f
Afras Rahim <afrasrahim11@gamil.com>
2025-05-11 19:50:23 +0530

    Routing, Dashboard, Layout and UI, Auth functionality done

---
+++ app/routes/admin/all-users.tsx
@@ -0,0 +1,16 @@
+import { Header } from "../../../components";
+
+const AllUsers = () => {
+  return (
+    <main className="dashboard wrapper">
+      <Header
+        title="Manage Users"
+        description="Filter, sort, and access detailed user profiles"
+      />
+
+      All Users Page Contents
+    </main>
+  )
+}
+
+export default AllUsers
